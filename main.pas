1b3050bcee49ed5dd2644c14072b56b7491300a2
fabalcu97 <fballoncuadros@gmail.com>
2017-06-28 02:44:03 -0500

    Added simpson to main

--- main.pas
+++ main.pas
@@ -12,6 +12,7 @@ var
     fixedPointSolver: TFixedPoint;
     dormandPrinceSolver: TDormandPrince;
     generalizedNewtonSolver: TGeneralizedNewton;
+    simpsonSolver: TSimpson;
     equation: String;
     derivativeEquation: String;
 
@@ -22,15 +23,18 @@ begin
     derivativeEquation := 'x*power(y, 2)';
 
     generalizedNewtonSolver := TGeneralizedNewton.Create();
-    dormandPrinceSolver := TDormandPrince.create(derivativeEquation, 0, 1, 0, 1, 0.1);
-    (*fixedPointSolver := TFixedPoint.Create(equation, derivativeEquation, 0.5, 0.0001);
+    (*dormandPrinceSolver := TDormandPrince.create(derivativeEquation, 0, 1, 0, 1, 0.1);
+    fixedPointSolver := TFixedPoint.Create(equation, derivativeEquation, 0.5, 0.0001);
     newtonSolver := TNewton.Create(equation, derivativeEquation, -1, 0.0001);
     secantSolver := TSecant.Create(equation, -1, 0.0001);
     bisectionSolver := TBisection.Create(equation, -1, 1, 0.0001);
     falsePositionSolver := TFalsePosition.Create(equation, -1, 1, 0.0001);*)
+    simpsonSolver := TSimpson.Create('power(x, 2)', -1, 0, 15);
 
+    F.Print(generalizedNewtonSolver.execute());
+    (*F.Print(simpsonSolver.simpson38());
     F.Print(dormandPrinceSolver.execute());
-    (*F.Print(fixedPointSolver.execute());
+    F.Print(fixedPointSolver.execute());
     F.Print(secantSolver.execute());
     F.Print(newtonSolver.execute());
     F.Print(bisectionSolver.execute());
